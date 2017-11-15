025cb205b8547a3ee8707f9c4af0f5c4c7fffa8d
Kakek Coding <32747943+kakekcoding@users.noreply.github.com>
2017-11-15 11:38:20 +0700

    Add files via upload

---
+++ app/controllers/contact.php
@@ -0,0 +1,15 @@
+<?php 
+	class Contact extends Controller
+	{
+		
+		public function index()
+		{
+			echo "Contact index";
+		}
+
+		public function phone()
+		{
+			echo "Contact by phone";
+		}
+	}
+?>
---
+++ app/view/home/index.php
@@ -0,0 +1,1 @@
+Hallo <?=$data['name'] ?>
